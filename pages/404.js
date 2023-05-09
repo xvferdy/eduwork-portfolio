2269119b8b89ede4883df096cc4b49453b895309
xvferdy <xv.ferdy@gmail.com>
2023-05-09 19:24:35 +0700

    ✨ [feature]: 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function FourOhFour() {
+  return (
+    <main id="main">
+      <div class="fof">
+        <h1>404</h1>
+      </div>
+    </main>
+  );
+}

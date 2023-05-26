6d56c9dabb6b5dd07674eeda9c28ebe653411a82
xvferdy <xv.ferdy@gmail.com>
2023-05-26 18:14:57 +0700

    🩹 [simple-fix]: font property

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import { Raleway } from "@next/font/google";
 const raleway = Raleway({
   weight: ["300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
-  // display: "swap",
+  display: "swap",
 });
 
 export default function App({ Component, pageProps }) {
